b7fc1d1438339a15285e262fe5864c4e3fc0b373
unknown <aleksimpellikka@gmail.com>
2023-12-04 00:03:27 +0200

    Add NavHost to RunNavGraph and HomeScreen as startDest

--- app/src/main/java/com/apellikka/runmint/RunNavGraph.kt
+++ app/src/main/java/com/apellikka/runmint/RunNavGraph.kt
@@ -3,14 +3,23 @@ package com.apellikka.runmint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.apellikka.runmint.ui.composables.HomeScreen
 
 @Composable
 fun RunNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-
     )
 {
-
+    NavHost(
+        navController = navController ,
+        startDestination = RunDestinations.HOME_ROUTE
+    ) {
+      composable(RunDestinations.HOME_ROUTE) {
+          HomeScreen()
+      }
+    }
 }
