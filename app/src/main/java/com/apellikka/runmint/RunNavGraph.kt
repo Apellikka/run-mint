68af931567d36f4be8606f48ab7a2ffbf12773d7
unknown <aleksimpellikka@gmail.com>
2023-12-03 22:40:55 +0200

    Add NavigationGraph file

---
+++ app/src/main/java/com/apellikka/runmint/RunNavGraph.kt
@@ -0,0 +1,16 @@
+package com.apellikka.runmint
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun RunNavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+
+    )
+{
+
+}
