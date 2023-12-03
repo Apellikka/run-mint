7915957de2f5c8f0276c1f93008b9f548f10126f
unknown <aleksimpellikka@gmail.com>
2023-12-04 00:05:08 +0200

    Add RunNavigation file7

---
+++ app/src/main/java/com/apellikka/runmint/RunNavigation.kt
@@ -0,0 +1,12 @@
+package com.apellikka.runmint
+
+import com.apellikka.runmint.RunScreens.HOME_SCREEN
+
+private object RunScreens {
+    const val HOME_SCREEN = "home"
+}
+
+object RunDestinations {
+    const val HOME_ROUTE = HOME_SCREEN
+}
+
