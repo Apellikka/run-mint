c8395e1fd74c7afd99a3fafb60b9af5a3a391224
unknown <aleksimpellikka@gmail.com>
2023-12-08 23:39:33 +0200

    Add stalinist font family and override some mat theme default textstyles

--- app/src/main/java/com/apellikka/runmint/ui/theme/Type.kt
+++ app/src/main/java/com/apellikka/runmint/ui/theme/Type.kt
@@ -2,33 +2,30 @@ package com.apellikka.runmint.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.apellikka.runmint.R
+
+val Stalinist = FontFamily(
+    Font(R.font.stalinist_one_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+    displayLarge = TextStyle(
+        fontFamily = Stalinist,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 28.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    labelLarge = TextStyle(
+        fontFamily = Stalinist,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
