dbf61612b7d9025d5b832ecd3dc0c8d3d43fc97f
unknown <aleksimpellikka@gmail.com>
2024-03-09 21:03:36 +0200

    Add navigation for ADD_RUN_SCREEN and create class for navi actions

--- app/src/main/java/com/apellikka/runmint/RunNavigation.kt
+++ app/src/main/java/com/apellikka/runmint/RunNavigation.kt
@@ -1,12 +1,45 @@
 package com.apellikka.runmint
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import com.apellikka.runmint.RunScreens.ADD_RUN_SCREEN
 import com.apellikka.runmint.RunScreens.HOME_SCREEN
 
 private object RunScreens {
     const val HOME_SCREEN = "home"
+    const val ADD_RUN_SCREEN = "add_run"
 }
 
 object RunDestinations {
     const val HOME_ROUTE = HOME_SCREEN
+    const val ADD_RUN_ROUTE = ADD_RUN_SCREEN
 }
+
+class RunNavigationActions(private val navController: NavController) {
+    fun navigateToAddRunScreen() {
+        navController.navigate(
+            ADD_RUN_SCREEN
+        ) {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = false
+            }
+            launchSingleTop = true
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
