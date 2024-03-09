20e923cec7625210f3989ac559cf98cbe86083a6
unknown <aleksimpellikka@gmail.com>
2024-03-09 21:03:54 +0200

    Add run screen to graph

--- app/src/main/java/com/apellikka/runmint/RunNavGraph.kt
+++ app/src/main/java/com/apellikka/runmint/RunNavGraph.kt
@@ -1,17 +1,22 @@
 package com.apellikka.runmint
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.apellikka.runmint.ui.composables.AddRunScreen
 import com.apellikka.runmint.ui.composables.HomeScreen
 
 @Composable
 fun RunNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
+    navActions: RunNavigationActions = remember(navController) {
+        RunNavigationActions(navController)
+    }
     )
 {
     NavHost(
@@ -19,7 +24,10 @@ fun RunNavGraph(
         startDestination = RunDestinations.HOME_ROUTE
     ) {
       composable(RunDestinations.HOME_ROUTE) {
-          HomeScreen()
+          HomeScreen(navActions)
+      }
+      composable(RunDestinations.ADD_RUN_ROUTE) {
+          AddRunScreen()
       }
     }
 }
