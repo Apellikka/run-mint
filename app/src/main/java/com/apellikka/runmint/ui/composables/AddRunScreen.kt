cfea8367489ae7c0dddec7aad3ccfbbdcb7c5d44
unknown <aleksimpellikka@gmail.com>
2024-03-17 16:24:08 +0200

    Validate distance, hour and minute input when adding a new run

--- app/src/main/java/com/apellikka/runmint/ui/composables/AddRunScreen.kt
+++ app/src/main/java/com/apellikka/runmint/ui/composables/AddRunScreen.kt
@@ -24,11 +24,19 @@ import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.apellikka.runmint.viewmodels.AddRunViewModel
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddRunScreen() {
+fun AddRunScreen(
+    addRunViewModel: AddRunViewModel = viewModel()) {
+
+    //TODO: Next order of business is to validate the inputs and add
+    // the remaining fields.
+    // TODO: Add runTypes to an array in viewmodel
+
     var selectedRunType by remember { mutableStateOf("") }
     var distance by remember { mutableStateOf("") }
     var hours by remember { mutableStateOf("") }
@@ -87,7 +95,8 @@ fun AddRunScreen() {
         }
         OutlinedTextField(
             value = distance,
-            onValueChange = { distance = it },
+            onValueChange = {
+                distance = addRunViewModel.validateDistanceInput(it) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Decimal,
                 imeAction = ImeAction.Next
@@ -104,7 +113,8 @@ fun AddRunScreen() {
         )
         OutlinedTextField(
             value = hours,
-            onValueChange = { hours = it },
+            onValueChange = {
+                hours = addRunViewModel.validateHourInput(it) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Decimal,
                 imeAction = ImeAction.Next
@@ -121,7 +131,7 @@ fun AddRunScreen() {
         )
         OutlinedTextField(
             value = minutes,
-            onValueChange = { minutes = it },
+            onValueChange = { minutes = addRunViewModel.validateMinuteInput(it) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Decimal,
                 imeAction = ImeAction.Next
