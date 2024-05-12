058b415b086028216787247c7eb906a0395ba296
unknown <aleksimpellikka@gmail.com>
2024-05-12 21:33:17 +0300

    Add run to DB

--- app/src/main/java/com/apellikka/runmint/database/entity/Run.kt
+++ app/src/main/java/com/apellikka/runmint/database/entity/Run.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "runs")
 data class Run(
-    @PrimaryKey val date: String,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val date: String,
     val runType: String,
     val distance: Double,
     val hours: Int,
