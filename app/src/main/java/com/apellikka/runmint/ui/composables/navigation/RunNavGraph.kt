c6c167b4b25189578621933651f2570563c34825
unknown <aleksimpellikka@gmail.com>
2024-06-12 21:52:53 +0300

    Add new screen destinations to RunNavGraph

--- app/src/main/java/com/apellikka/runmint/ui/composables/navigation/RunNavGraph.kt
+++ app/src/main/java/com/apellikka/runmint/ui/composables/navigation/RunNavGraph.kt
@@ -5,13 +5,13 @@ import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.apellikka.runmint.ui.composables.AddRunScreen
 import com.apellikka.runmint.ui.composables.HomeScreen
+import com.apellikka.runmint.ui.composables.PlaceHolder
 
 @Composable
 fun RunNavGraph(
-    navController: NavHostController = rememberNavController(),
+    navController: NavHostController,
     navActions: RunNavigationActions = remember(navController) {
         RunNavigationActions(navController)
     }
@@ -21,11 +21,20 @@ fun RunNavGraph(
         navController = navController ,
         startDestination = RunDestinations.HOME_ROUTE
     ) {
-      composable(RunDestinations.HOME_ROUTE) {
-          HomeScreen(navActions)
+        composable(RunDestinations.HOME_ROUTE) {
+            HomeScreen(navActions)
       }
-      composable(RunDestinations.ADD_RUN_ROUTE) {
-          AddRunScreen()
+        composable(RunDestinations.ADD_RUN_ROUTE) {
+            AddRunScreen()
       }
+        composable(RunDestinations.LOG_ROUTE) {
+            PlaceHolder()
+        }
+        composable(RunDestinations.PLAN_ROUTE) {
+            PlaceHolder()
+        }
+        composable(RunDestinations.STATS_ROUTE) {
+            PlaceHolder()
+        }
     }
 }
--- app/src/main/java/com/apellikka/runmint/ui/composables/navigation/RunNavigation.kt
+++ app/src/main/java/com/apellikka/runmint/ui/composables/navigation/RunNavigation.kt
@@ -4,15 +4,24 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.apellikka.runmint.ui.composables.navigation.RunScreens.ADD_RUN_SCREEN
 import com.apellikka.runmint.ui.composables.navigation.RunScreens.HOME_SCREEN
+import com.apellikka.runmint.ui.composables.navigation.RunScreens.LOG_SCREEN
+import com.apellikka.runmint.ui.composables.navigation.RunScreens.PLAN_SCREEN
+import com.apellikka.runmint.ui.composables.navigation.RunScreens.STATS_SCREEN
 
 private object RunScreens {
     const val HOME_SCREEN = "home"
     const val ADD_RUN_SCREEN = "add_run"
+    const val LOG_SCREEN = "log"
+    const val PLAN_SCREEN = "plan"
+    const val STATS_SCREEN = "stats"
 }
 
 object RunDestinations {
     const val HOME_ROUTE = HOME_SCREEN
     const val ADD_RUN_ROUTE = ADD_RUN_SCREEN
+    const val LOG_ROUTE = LOG_SCREEN
+    const val PLAN_ROUTE = PLAN_SCREEN
+    const val STATS_ROUTE = STATS_SCREEN
 }
 
 class RunNavigationActions(private val navController: NavController) {
