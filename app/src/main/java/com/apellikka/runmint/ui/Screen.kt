717e4e659c7c7981bee59a8bc4524e9133cf7645
unknown <aleksimpellikka@gmail.com>
2024-06-16 17:10:17 +0300

    Add data class for weekly stats

--- app/src/main/java/com/apellikka/runmint/ui/Screen.kt
+++ app/src/main/java/com/apellikka/runmint/ui/Screen.kt
@@ -1,8 +1,12 @@
 <<<<<<< HEAD:app/src/main/java/com/apellikka/runmint/ui/composables/navigation/Screen.kt
+<<<<<<< HEAD:app/src/main/java/com/apellikka/runmint/ui/composables/navigation/Screen.kt
 package com.apellikka.runmint.ui.composables.navigation
 =======
 package com.apellikka.runmint.ui
 >>>>>>> c21757e (fixup! Add functionality for getting run stats WIP):app/src/main/java/com/apellikka/runmint/ui/Screen.kt
+=======
+package com.apellikka.runmint.activities
+>>>>>>> a44e724 (Add data class for weekly stats):app/src/main/java/com/apellikka/runmint/ui/Screen.kt
 
 import androidx.annotation.StringRes
 import com.apellikka.runmint.R
